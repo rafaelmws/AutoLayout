2bac56ed90c147ca9c03ef36386ab739770b6802
Rafael Martins <rafael.martins@corp.globo.com>
2015-11-27 20:10:20 -0200

    autolayout ajust

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -10,5 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+    
+    var secure = false { didSet{ updateUI() } }
+    
+    private func updateUI() {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    @IBAction func changeSecurity(sender: UIButton) {
+        secure = !secure
+    }
+    
+    @IBAction func login() {
+    }
 }
